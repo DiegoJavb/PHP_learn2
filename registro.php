d0a305736592ec7c903a26f774c309e1b41c4e41
Diego <diegohd-20@outlook.com>
2022-06-01 10:14:21 -0500

    inicializacion de proyecto

---
+++ registro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <title>registro</title>
+</head>
+
+<body>
+
+    <div class="contenedor">
+        <nav>
+            <h1>Bienvenido</h1>
+        </nav>
+        <div class="seccion_tarjeta">
+            <div class="tarjeta">
+                <form action="validacion.php" method="post">
+                    <h2>Registro</h2><br><br>
+                    <label for="usuario">Usuario</label><br>
+                    <input type="text" name="usuario" require><br><br>
+                    <label for="contraseña">Contraseña</label><br>
+                    <input type="contraseña" name="contrasena" require><br><br>
+                    <div style="height: 30px;"></div>
+                    <input class="submit" type="submit" value="Registrate"><br><br>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
