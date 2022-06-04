685464d14a59846c8fcc4611ad754115fb722d10
Diego <diegohd-20@outlook.com>
2022-06-03 19:30:22 -0500

    grid de home page

---
+++ home.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=}, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/home.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="contenedor">
+        <header>
+            <nav class="menu">
+                <ul class="lista1">
+                    <li>
+                        <a href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Contactos</a>
+                    </li>
+                    <li>
+                        <a href="#">Sobre Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="#">Usuarios</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <nav class="derecha">
+            <h2 class="nav">Noticias</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae velit
+            quos vel cum sed, illo repellat est voluptates exercitationem, quod nihil magnam
+            voluptatum aliquid eaque? Quasi vitae odit blanditiis culpa.
+            <br><br>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. A ipsa, adipisci
+            necessitatibus voluptate enim voluptatum, corporis dolore delectus quibusdam iure perspiciatis.
+            Natus doloribus rerum itaque non aliquam veritatis corrupti exercitationem?
+            <br><br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, dolor asperiores! Omnis,
+            fugiat aliquam? Quasi dolorem, assumenda expedita consectetur nisi
+            nulla eveniet maiores minus numquam adipisci velit? Cupiditate, nulla minima!
+            <br><br>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, sed
+            architecto quas sit, delectus quam aliquam optio esse autem quia
+            velit itaque! Eaque impedit aspernatur harum error animi enim inventore!
+        </nav>
+        <section class="section1">
+            <h2 class="section">Novedades</h2>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+            provident eaque doloribus rerum hic esse error distinctio ipsa exercitationem
+            earum suscipit! Pariatur, ipsum iure? Beatae, autem. Obcaecati dolores labore sed!
+            <br><br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe odio
+            culpa, reiciendis pariatur recusandae perferendis eius ullam facere
+            delectus eligendi blanditiis earum sunt minima ut numquam omnis, autem sit iusto.
+            <br><br>
+            lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quidem, quisquam.
+            <br><br>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque ullam, odio inventore vitae
+            rerum, eum neque totam, impedit necessitatibus distinctio pariatur
+            ipsam deserunt iusto sapiente sequi animi natus voluptate in!
+        </section>
+
+
+        <article class="article1">
+            <h2>Curiosidades</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ut amet,
+            nihil quas asperiores explicabo. Tenetur maxime est pariatur rem dolores
+            neque rerum numquam officia expedita minima. Quod, omnis maxime.
+            <br><br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque atque quaerat,
+            optio obcaecati, molestias neque delectus omnis minima cupiditate porro officia
+            assumenda quis facilis commodi eligendi, iste maxime cum ad?
+            <br><br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi eligendi quo, aliquam
+            nisi nam dignissimos libero hic, ut aut perferendis, eius quod rem voluptatum.
+            Corporis temporibus iste animi officiis ea.
+            <br><br>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus tempora
+            labore facere. Ipsum, officiis a. Omnis corrupti error esse, repellendus nobis
+            quam corporis quae perspiciatis quia, tenetur dolorum impedit. Magni.
+        </article>
+    </div>
+</body>
+
+</html>
